4b5cc1e6c181a2f1d8b123ab33716431c6ec5c6c
Paula <pfr.blackcat@gmail.com>
2022-06-27 19:41:03 -0300

    aula-5-final2

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -66,7 +66,7 @@ export function Subscribe() {
         </div>
 
       </div>
-      <img src="/src/assets/code-mockup.png" className="subscribeMockup" alt="" />
+      <img src="/public/image/code-mockup.png" className="subscribeMockup" alt="" />
     </div>
   );
 }
